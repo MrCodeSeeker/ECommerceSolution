644fcd6084b38b101db7c43fabc270493ad90d6b
MrCodeSeeker <praveen385p@gmail.com>
2026-05-03 13:06:19 +0530

    feat(persistence): configure Product entity in DbContext and initial migration

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,3 +1,7 @@
+using ECommerce.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 namespace ECommerce.API
 {
     public class Program
@@ -5,6 +9,10 @@ namespace ECommerce.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<AppDbContext>(Options =>
+            {
+                Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found."));
+            });
 
             // Add services to the container.
 
--- ECommerce.Persistence/AppDbContext.cs
+++ ECommerce.Persistence/AppDbContext.cs
@@ -4,10 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ECommerce.Domain.Entities;
 namespace ECommerce.Persistence
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Product> Products{set; get;}
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.Price).HasColumnType("decimal(18,2)");
+                entity.Property(p => p.Stock)
+                 .IsRequired();
+            });
+        }
     }
 }
